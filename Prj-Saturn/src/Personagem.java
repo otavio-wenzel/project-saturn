b847b605c47d613b7cff61739d2e71febf0d5821
otavio-wenzel <otavio.wenzel@@gmail.com>
2024-06-18 00:36:20 -0300

    Adicionando os primeiros atributos à superclasse personagem

---
+++ Prj-Saturn/src/Personagem.java
@@ -0,0 +1,12 @@
+public abstract class Personagem{
+    protected String nome;
+    protected int idade;
+    protected String genero;
+    
+    protected int forca;
+    protected int destreza;
+    protected int constituicao;
+    protected int inteligencia;
+    protected int sabedoria;
+    protected int carisma;
+}
