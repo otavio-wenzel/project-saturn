a8ce5aa20b1ff92b535c5f5f6dc3e19ad3c65b72
otavio-wenzel <otavio.wenzel@gmail.com>
2024-06-24 20:04:48 -0300

    Incluindo tratamento de excessão na classe Sistema para criação e ou leitura de arquivo de persistencia

--- Prj-Saturn/src/Sistema.java
+++ Prj-Saturn/src/Sistema.java
@@ -1,26 +1,26 @@
-//import java.io.IOException;
+import java.io.IOException;
 
 public class Sistema {
 
     private static void exibirMenu() {
         
-        System.out.println("\nGerenciamento de Obras de Arte do Museu");
-        System.out.println("1) Cadastrar");
-        System.out.println("2) Buscar por Título");
-        System.out.println("3) Apagar");
-        System.out.println("4) Listar todas as obras");
+        System.out.println("\nMENU DE PERSONAGENS");
+        System.out.println("1) Criar");
+        System.out.println("2) Buscar pelo Nome");
+        System.out.println("3) Excluir");
+        System.out.println("4) Listar todos os Personagens");
         System.out.println("0) Sair");
     }
 
     public static void executar() {
 
-       /*try {
-            GerenciadorObras.criarArquivoSeNaoExistir();
-            GerenciadorObras.lerArquivo();
+       try {
+            GerenciadorDePersonagens.criarArquivoSeNaoExistir();
+            GerenciadorDePersonagens.lerArquivo();
     
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
-        }*/
+        }
 
         while (true) {
             
