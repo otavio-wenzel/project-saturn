342110e0daed505aa3e080f8b237149da01a28a2
otavio-wenzel <otavio.wenzel@gmail.com>
2024-06-23 21:43:08 -0300

    Adicinando opções do sistema e menu de navegação

---
+++ Prj-Saturn/src/Sistema.java
@@ -0,0 +1,42 @@
+//import java.io.IOException;
+
+public class Sistema {
+
+    private static void exibirMenu() {
+        
+        System.out.println("\nGerenciamento de Obras de Arte do Museu");
+        System.out.println("1) Cadastrar");
+        System.out.println("2) Buscar por Título");
+        System.out.println("3) Apagar");
+        System.out.println("4) Listar todas as obras");
+        System.out.println("0) Sair");
+    }
+
+    public static void executar() {
+
+       /*try {
+            GerenciadorObras.criarArquivoSeNaoExistir();
+            GerenciadorObras.lerArquivo();
+    
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }*/
+
+        while (true) {
+            
+            exibirMenu();
+            int op = Console.lerInt("Informe uma opção");
+            
+            try {
+                GerenciadorDePersonagens.verificarOpcaoDigitada(op);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+
+    }
+
+    
+
+}
